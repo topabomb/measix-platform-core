b0370d73de97f8e0b2624de2f28294df540d3fa8
Topabomb <hualei.hb@gmail.com>
2026-08-19 16:39:35 +0800

    feat: make HTTP shutdown grace explicit

--- backend/internal/common/server/server.go
+++ backend/internal/common/server/server.go
@@ -8,11 +8,25 @@ import (
 	"time"
 )
 
-type HTTP struct{ Server *http.Server }
+type HTTP struct {
+	Server        *http.Server
+	ShutdownGrace time.Duration
+}
 
 func New(addr string, handler http.Handler) *HTTP {
-	return &HTTP{Server: &http.Server{Addr: addr, Handler: handler, ReadHeaderTimeout: 10 * time.Second, IdleTimeout: 120 * time.Second}}
+	return NewWithGrace(addr, handler, 30*time.Second)
 }
+
+func NewWithGrace(addr string, handler http.Handler, shutdownGrace time.Duration) *HTTP {
+	if shutdownGrace <= 0 {
+		shutdownGrace = 30 * time.Second
+	}
+	return &HTTP{
+		Server:        &http.Server{Addr: addr, Handler: handler, ReadHeaderTimeout: 10 * time.Second, IdleTimeout: 120 * time.Second},
+		ShutdownGrace: shutdownGrace,
+	}
+}
+
 func (s *HTTP) Run(ctx context.Context, log *slog.Logger) error {
 	errc := make(chan error, 1)
 	go func() {
@@ -27,7 +41,7 @@ func (s *HTTP) Run(ctx context.Context, log *slog.Logger) error {
 	case err := <-errc:
 		return err
 	case <-ctx.Done():
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), s.ShutdownGrace)
 		defer cancel()
 		return s.Server.Shutdown(shutdownCtx)
 	}
